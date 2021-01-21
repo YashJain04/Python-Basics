fdc08e1aeceb98c95d52447f25702abd17c0867c
Yash Jain <73800643+YashJain04@users.noreply.github.com>
2021-01-21 10:41:20 -0500

    Add files via upload

---
+++ Activity #3 - Learning Sets/Sets.py
@@ -0,0 +1,112 @@
+# TO UNCOMMENT CODE - SELECT ALL (CTRL + A) - THEN (CTRL + /)
+
+# #Section 1
+# print("Section 1" + "\n")
+
+# #create an empty list
+# types_of_fruits = []
+
+# #create an integer type variable that asks the user the # of items they would like to enter
+# number_of_fruits = int(input("Enter the number of fruits you would like to add to the list: "))
+# print() #printing empty line
+
+# #for loop that starts from the 0th value to the value the user entered for the variable "number_of_fruits"
+# for i in range (0, number_of_fruits):
+#     fruits = str(input()) #gathering user input and storing the users values in a string type variable called fruits
+#     types_of_fruits.append(fruits) #adding the value to the list using the append function
+
+# print() #printing empty line
+# print(types_of_fruits) #printing the list
+
+# #Section 2
+# print("\n" + "Section 2" + "\n")
+
+# #creating a set
+# cars = {"mazda", "honda", "lambo"}
+
+# #printing a set
+# print(cars)
+
+# #Section 3
+# print("\n" + "Section 3" + "\n")
+
+# #creating duplicate values within sets
+# cars = {"mazda", "honda", "mazda"}
+# countries = {"Canada", "USA", "China", "China", "USA", "Mexico"}
+
+# #printing the sets
+# print(cars)
+# print()
+# print(countries)
+
+# #Section 4
+# print("\n" + "Section 4" + "\n")
+
+# #creating a set
+# cars = {"mazda", "honda", "mazda"}
+
+# #printing a specific index of the set
+# print(cars[1])
+# print(cars[2])
+# print(cars[3])
+
+# #Section 5
+# print("\n" + "Section 5" + "\n")
+
+# #set of vowels
+# vowels = {'a', 'e', 'i', 'u'}
+
+# #adding 'o'
+# vowels.add('o')
+# print('Vowels are:', vowels)
+
+# #adding 'a' again
+# vowels.add('a')
+# print('Vowels are:', vowels)
+
+# #Section 6
+# print("\n" + "Section 6" + "\n")
+
+# #language set
+# language = {'English', 'French', 'German'}
+
+# #removing 'German' from language
+# language.remove('German')
+
+# #updated language set
+# print('Updated language set:', language)
+
+# #animal set
+# animal = {'cat', 'dog', 'rabbit', 'guinea pig'}
+
+# #deleting 'fish' element
+# animal.remove('fish')
+
+# #updated animal
+# print('Updated animal set:', animal)
+
+# #Section 7
+# print("\n" + "Section 7" + "\n")
+
+# #creating sets with different values
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {1, 5, 7, 9, 3}
+# set3 = {True, False, False}
+# set4 = {"abc", 34, True, 40, "male"}
+
+# #printing a set
+# print(set1)
+# print(set2)
+# print(set3)
+# print(set4)
+
+# #Section 8
+# print("\n" + "Section 8" + "\n")
+
+# #creating sets using the set() method
+# cars = set({"mazda", "honda", "lambo"}) #uses curly brackets
+# foods = set(("Pizza", "Pasta", "Salads")) #uses round brackets
+
+# #printing the sets
+# print(cars)
+# print(foods)
